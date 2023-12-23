84221e9dd08649ec23dc42e32c2f8f4780283fe6
Polunkkkka <dolmatovap.2021@mail.ru>
2023-12-23 22:15:18 +0300

    Добавьте файлы проекта.

---
+++ WordsFromSentence2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsFromSentence2; 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите предложение: ");
+        string input = Console.ReadLine();
+        string[] words = SplitText(input);
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+        Console.ReadKey();
+    }
+
+    /// <summary>
+    /// Этот метод разбивает введенное предложение на отдельные слова и сохраняет их в массив words
+    /// </summary>
+    static string[] SplitText(string text)
+    {
+        string[] words = text.Split(' ');
+        return words;
+    }
+}
